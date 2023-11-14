9214dabb37b95872cf6639851e91db88e8e3d711
jeanPierreR99 <kreyesf@unamad.edu.pe>
2023-11-14 09:10:29 -0500

    add content

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
+
+
+// curl -H "Content-Type: application/json" \
+//      -H "Authorization: Bearer F6E7DFE46F336E194DBE5E858908BEF05A04C5795E85C65619BF52404D7A0224" \
+//      -X POST "https://b17646f6-89ff-4959-83cd-ec8e141b6afc.pushnotifications.pusher.com/publish_api/v1/instances/b17646f6-89ff-4959-83cd-ec8e141b6afc/publishes" \
+//      -d '{"interests":["hello"],"web":{"notification":{"title":"FUNCIONA!!!!","body":"UN BAÑO Y A MIMIR JAJA"}}}'
